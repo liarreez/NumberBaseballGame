4a14dcfabe9ce0f10dbf6fa3811b26156c5876d1
liarreez <liarreez@gmail.com>
2023-02-23 18:11:31 +0900

    feat(Computer) : generate random number(without duplicate)

--- src/main/java/numberbaseballgame/Application.java
+++ src/main/java/numberbaseballgame/Application.java
@@ -3,6 +3,8 @@ package numberbaseballgame;
 public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer();
+
         computer.printStartMessage();
+        computer.generateRandomNumber();
     }
 }
--- src/main/java/numberbaseballgame/Computer.java
+++ src/main/java/numberbaseballgame/Computer.java
@@ -2,7 +2,31 @@ package numberbaseballgame;
 
 public class Computer {
 
+    private final int NUMBER_LENGTH = 4;
+    private final int MAX_NUM = 9;
+
+    private int[] number = new int[NUMBER_LENGTH];
+
     public void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void generateRandomNumber () {
+        int tmpNum = 0;
+        for(int i=0; i < NUMBER_LENGTH; i++ ) {
+            do {
+                tmpNum = (int) ((Math.random() * MAX_NUM) + 1);
+            } while(duplicateNumCheck(number, tmpNum));
+            number[i] = tmpNum;
+        }
+    }
+
+    private boolean duplicateNumCheck(int[] number, int num) {
+        for(int i=0; i < NUMBER_LENGTH; i++ ) {
+            if (number[i] == num) {
+                return true;
+            }
+        }
+        return false;
     }
 }
